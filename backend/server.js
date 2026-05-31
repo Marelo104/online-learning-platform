e5d4fee5a816b638bd25aabf5e59b14719ef3e0a
Marelo104 <yanninckaltay@gmail.com>
2026-05-31 15:24:09 +0100

    project completed

--- backend/server.js
+++ backend/server.js
@@ -29,11 +29,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+// app.get("/", (req, res) => {
+  //   res.send("Welcome to the Online Learning Platform API!");
+  // });
+  
 // Routes
-app.get("/", (req, res) => {
-  res.send("Welcome to the Online Learning Platform API!");
-});
-
 app.use("/api/auth", authRoutes);
 app.use("/api/courses", courseRoutes);
 app.use("/api/courses", lessonRoutes); // nested under courses for context
